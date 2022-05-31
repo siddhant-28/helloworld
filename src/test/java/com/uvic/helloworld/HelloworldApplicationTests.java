559dd61cf3ea5cb327d14a435cf3dc698a3d3c6c
Siddhant Kumar <siddhant.7000@gmail.com>
2022-05-31 16:50:43 -0700

    initial commit

---
+++ src/test/java/com/uvic/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvic.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
